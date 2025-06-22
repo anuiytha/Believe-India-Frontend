0a9289abbc93633cb06e5b271c18ded90a6a4141
221910310008 <221910310008@gitam.in>
2025-06-22 19:31:46 -0400

    changed submit idea to a different page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -77,7 +77,7 @@ const Navbar = () => {
             path: '/idea-bank',
             hasDropdown: true,
             dropdownItems: [
-                { name: 'Submit Idea', path: '/idea-bank/submit-idea' },
+                { name: 'Submit Idea', path: '/submit-idea' },
                 { name: 'Browse Ideas', path: '/idea-bank' },
                 { name: 'Featured Ideas', path: '/idea-bank/featured' },
                 { name: 'Idea Guidelines', path: '/idea-bank/guidelines' }
